5e2d861b8900672ee1a12e904840c7074aac0c9e
Saurabh Bhatt <bhattsourav4@gmail.com>
2025-02-20 22:42:01 +0530

    add all middlewares

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,41 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+
+
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+
+    try {
+
+        // mobileUser
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        if (!token) {
+            throw new ApiError(401, "unauthorized user")
+        }
+
+        // user k pass jo bhi token diya jata h wo encrypted hota h
+        // usko DB wale refresh token se compare krne se pehle decode krna pdta h
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findbyID(decodedToken?._id).select("-password -refreshtoken")
+
+        if (!user) {
+            throw new ApiError(401, "Invalid accessToken")
+        }
+
+        req.user = user
+        next()
+
+
+    } catch (error) {
+        throw new ApiError(401, error.message || "Inavalid accessToken")
+    }
+
+})
+
+
+
