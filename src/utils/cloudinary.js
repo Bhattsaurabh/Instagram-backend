1c9c1ed41979380e029efaa8d87d9713b23e0e09
Saurabh Bhatt <bhattsourav4@gmail.com>
2025-02-21 23:39:48 +0530

    add file upload method

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,8 @@
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"     //file system
 import dotenv from "dotenv"
+import { asyncHandler } from "./asyncHandler";
+import { ApiError } from "./ApiError";
 
 
 dotenv.config({
@@ -13,3 +15,34 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret:process.env.CLOUDINARY_API_SECRET
 });
+
+
+const uploadOnCloudinary = async(localPath) =>{
+
+
+    try {
+        if(!localPath)
+        {
+            return null
+        }
+    
+        const response = await cloudinary.uploader.upload(localPath, {
+                resource_type: "auto"
+            })
+        
+        fs.unlink(localPath)
+    
+        if(!response)
+        {
+            throw new ApiError(400, "something went wrong failed to upload file")
+        }
+    
+        return response
+    } catch (error) {
+        throw new ApiError(500, "Internal server error failed to upload file")
+    }
+
+
+}
+
+export {uploadOnCloudinary}
